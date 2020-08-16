d0f473ed5c9213513c1f01a604f048eace19c4cb
manishsaraan <manish021js@gmail.com>
2020-08-17 00:05:40 +0530

    port updated for heroku

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3030;
+const port = process.env.PORT || 3030;
 const bodyParser = require('body-parser');
 const oauthServer = require('./oauth/server.js');
 const debug = require('./utilities/debug.js');

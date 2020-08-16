95bca3de803a97bf43dbe348ff958413d12909e2
manishsaraan <manish021js@gmail.com>
2020-08-17 00:13:49 +0530

    add heroku urls to make it work

--- app/oauth/model.js
+++ app/oauth/model.js
@@ -40,7 +40,7 @@ module.exports = {
       clientId: clientId,
       clientSecret: clientSecret,
       grants: ['authorization_code', 'refresh_token'],
-      redirectUris: ['http://localhost:3030/client/app'],
+      redirectUris: ['https://oauth-app-2.herokuapp.com/client/app'],
     };
     return new Promise((resolve) => {
       resolve(db.client);
